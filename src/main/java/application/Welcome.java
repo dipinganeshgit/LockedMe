cc7983d98ce25f8fe5b4331564a334466344682f
Dipin <dipinganesh89@gmail.com>
2023-06-21 19:13:39 +0400

    first commit

---
+++ src/main/java/application/Welcome.java
@@ -0,0 +1,28 @@
+package application;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Welcome welcome = new Welcome();
+		welcome.displayWelcomeString();
+	}
+
+	
+	void displayWelcomeString()
+	{
+		System.out.println("#                                                                                   \n"
+				+ "#   _ _ _     _                      _          __            _         _ _____     \n"
+				+ "#  | | | |___| |___ ___ _____ ___   | |_ ___   |  |   ___ ___| |_ ___ _| |     |___ \n"
+				+ "#  | | | | -_| |  _| . |     | -_|  |  _| . |  |  |__| . |  _| '_| -_| . | | | | -_|\n"
+				+ "#  |_____|___|_|___|___|_|_|_|___|  |_| |___|  |_____|___|___|_,_|___|___|_|_|_|___|\n"
+				+ "#                                                                                   ");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Press any key to continue");		
+		
+	}
+	
+}
