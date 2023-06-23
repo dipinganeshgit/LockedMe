41d88fd728584b5e87760f96925661b29f5e999f
Dipin <dipinganesh89@gmail.com>
2023-06-24 01:57:04 +0400

    Added screenshots

--- src/main/java/application/Welcome.java
+++ src/main/java/application/Welcome.java
@@ -13,16 +13,16 @@ public class Welcome {
 	void displayWelcomeString()
 	{
 
-		System.out.println("#                                                                                   \n"
-				+ "#   _ _ _     _                      _          __            _         _ _____     \n"
-				+ "#  | | | |___| |___ ___ _____ ___   | |_ ___   |  |   ___ ___| |_ ___ _| |     |___ \n"
-				+ "#  | | | | -_| |  _| . |     | -_|  |  _| . |  |  |__| . |  _| '_| -_| . | | | | -_|\n"
-				+ "#  |_____|___|_|___|___|_|_|_|___|  |_| |___|  |_____|___|___|_,_|___|___|_|_|_|___|\n"
-				+ "#                                                                                   ");
+		System.out.println("			#                                                                                   \n"
+				+ "			#   _ _ _     _                      _          __            _         _ _____     \n"
+				+ "			#  | | | |___| |___ ___ _____ ___   | |_ ___   |  |   ___ ___| |_ ___ _| |     |___ \n"
+				+ "			#  | | | | -_| |  _| . |     | -_|  |  _| . |  |  |__| . |  _| '_| -_| . | | | | -_|\n"
+				+ "			#  |_____|___|_|___|___|_|_|_|___|  |_| |___|  |_____|___|___|_,_|___|___|_|_|_|___|\n"
+				+ "			#                                                                                   ");
 		System.out.println("");
 		System.out.println("");
-		System.out.println("");
-		System.out.println("Press Enter key 'H' to go to home screen");		
+		System.out.println("							Developed by Dipin Ganesh");
+		System.out.println("						Press Enter key 'H' to go to home screen");		
 	
 		loadScanner();
 
